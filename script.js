839c011f05e5b9cf0501c47824ad1a500823d189
Vitor <vitoramorimseco@gmail.com>
2023-09-24 18:51:21 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //  documentElement -> entra na teg html
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")){
+    img.setAttribute('src', './assets/claro.png') 
+    img.setAttribute("alt", "Vitor no claro")
+    //setAttribute -> adicionar, ajutar e modificar um atributo
+    //se tiver light mode, adicionar a imagem light
+}
+else{
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', "./assets/Texto do seu parágrafo.png")
+    img.setAttribute('alt', 'Vitor no escuro')
+}
+
+}
+
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+  // contains -> ele pergunta se contem alguma classe procuranda no ()
